e1e8460f83e3b82c7b30dd7acfc539ef8bb8684c
JanZelent <janzelentlipinski@gmail.com>
2026-03-22 18:11:48 +0100

    Gotowy projekt.

--- NovitusApp/Platforms/Android/MainActivity.cs
+++ NovitusApp/Platforms/Android/MainActivity.cs
@@ -7,5 +7,15 @@ namespace NovitusApp
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            
+            Window.SetStatusBarColor(Android.Graphics.Color.White);
+
+            var decorView = Window.DecorView;
+            decorView.SystemUiVisibility = (Android.Views.StatusBarVisibility)Android.Views.SystemUiFlags.LightStatusBar;
+        }
     }
 }
---
+++ README.md
@@ -0,0 +1,60 @@
+\# NovitusApp
+
+
+
+Aplikacja mobilna stworzona w .NET MAUI, która wyświetla podstawowe informacje o urządzeniu z systemem Android.
+
+
+
+\## Funkcjonalności
+
+
+
+\- Producent urządzenia
+
+\- Model
+
+\- Pamięć RAM
+
+\- Pamięć wewnętrzna
+
+\- Próba pobrania numeru seryjnego
+
+\- ANDROID\_ID jako identyfikator
+
+
+
+\##Technologie
+
+
+
+\- .NET MAUI
+
+\- C#
+
+\- Android SDK
+
+
+
+\##  Numer seryjny
+
+
+
+Android 10+ blokuje dostęp do numeru seryjnego urządzenia.  
+
+Zastosowano ANDROID\_ID jako alternatywę.
+
+
+
+\## Testowanie
+
+
+
+\- Emulator Android
+
+\- Fizyczne urządzenie
+
+
+
+
+
